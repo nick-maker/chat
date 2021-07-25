d2e5e2121aa9dc47d03a24122e020af0ec2e0f07
Nick Liu <hybrida666@icloud.com>
2021-07-26 00:06:10 +0800

    done

---
+++ chat.py
@@ -0,0 +1,40 @@
+def readFile(fileName):
+	lines = []
+	with open(fileName, 'r') as f:
+		for line in f:
+			lines.append(line.strip().replace('\ufeff',''))
+	return lines
+
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	return new
+
+def writeFile(fileName, new):
+	with open(fileName, 'w') as f:
+		for line in new:
+			f.write(line + '\n')
+		
+		
+
+
+def main():
+	lines = readFile('input.txt')
+	new = convert(lines)
+	writeFile('output.txt', new)
+
+main()
+
+# with open ('output.txt', 'w') as f:
+
+# for words in lines:
+# 	if 'Allen' or 'Tom' in words:
